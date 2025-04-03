f51cbf1ff62713739a1b4af6fc29bd4ee14e9b95
Notcool404 <nishantdhuri404@gmail.com>
2025-04-04 02:14:48 +0530

    Update prac6b_contracts_inheritance_cons.py

--- prac6b_contracts_inheritance_cons.py
+++ prac6b_contracts_inheritance_cons.py
@@ -1,162 +1,189 @@
 #A)Contract:
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
 contract C {
-//private state variable
-uint private data;
+    // private state variable
+    uint256 private data;
 
+    // public state variable
+    uint256 public info;
 
-//public state variable
-uint public info;
+    // constructor
+    constructor() {
+        info = 10;
+    }
 
+    // private function
+    function increment(uint256 a) private pure returns (uint256) {
+        return a + 1;
+    }
 
-//constructor
-constructor() public {
-info = 10;
+    // public function
+    function updateData(uint256 a) public {
+        data = a;
+    }
+
+    function getData() public view virtual returns (uint256) {
+        return data;
+    }
+
+    function compute(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
 }
-//private function
-function increment(uint a) private pure returns(uint) { return a + 1; }
-//public function
-function updateData(uint a) public { data = a; }
-function getData() public view returns(uint) { return data; }
-function compute(uint a, uint b) internal pure returns (uint) { return a + b; }
-}
-//Derived Contract
+
+// Derived Contract
 contract E is C {
-uint private result;
-C private c;
+    uint256 private result;
 
+    function getComputedResult() public returns (uint256) {
+        result = compute(3, 5);
+        return result;
+    }
 
-constructor() public {
-c = new C();
-}
-function getComputedResult() public {
-result = compute(3, 5);
-}
-function getResult() public view returns(uint) { return result; }
-function getData() public view returns(uint) { return c.info(); }
+    function getResult() public view returns (uint256) {
+        return result;
+    }
+
+    function getData() public view override returns (uint256) {
+        return info;  // Accessing inherited public variable directly
+    }
 }
 
 
 
 #B)Inheritance:
-// Solidity program to
-// demonstrate
+// SPDX-License-Identifier: GPL-3.0 
 // Single Inheritance
-pragma solidity >=0.4.22 <0.6.0;
-
+pragma solidity ^0.8.0;
 
 // Defining contract
-contract parent{
-// Declaring internal
-// state variable
-uint internal sum;
+contract parent {
+    // Declaring internal
+    // state variable
+    uint256 internal sum;
 
+    // Defining external function
+    // to set value of internal
+    // state variable sum
+    function setValue() external {
+        uint256 a = 20;
+        uint256 b = 20;
+        sum = a + b;
+    }
+}
 
-// Defining external function
-// to set value of internal
-// state variable sum
-function setValue() external {
-uint a = 20;
-uint b = 20;
-sum = a + b;
-}
-}
 // Defining child contract
-contract child is parent{
-
-
-// Defining external function
-// to return value of
-// internal state variable sum
-function getValue() external view returns(uint) {
-return sum;
+contract child is parent {
+    // Defining external function
+    // to return value of
+    // internal state variable sum
+    function getValue() external view returns (uint256) {
+        return sum;
+    }
 }
-}
+
 // Defining calling contract
 contract caller {
+    // Creating child contract object
+    child cc = new child();
 
+    // Defining function to call
+    // setValue and getValue functions
+    function testInheritance() public {
+        cc.setValue();
+    }
 
-// Creating child contract object
-child cc = new child();
-
-
-// Defining function to call
-// setValue and getValue functions
-function testInheritance() public {
-cc.setValue();
+    function result() public view returns (uint256) {
+        return cc.getValue();
+    }
 }
-function result() public view returns(uint ){
-return cc.getValue();
-}
-}
+
 
 
 #C)Constructors:
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: GPL-3.0 
+pragma solidity ^0.8.0;
 contract Base {
-uint data;
-constructor(uint _data) public {
-data = _data;
+    uint256 data;
+
+    constructor(uint256 _data) public {
+        data = _data;
+    }
+
+    function getresult() public view returns (uint256) {
+        return data;
+    }
 }
-function getresult()public view returns(uint){
-return data;
+
+contract Derived is Base(5) {
+    constructor() public {}
 }
-}
-contract Derived is Base (5) {
-constructor() public {}
-}
+
 
 
 // Indirect Initialization of Base Constructor
-pragma solidity ^0.5.0;
-
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
 
 contract Base {
-uint data;
-constructor(uint _data) public {
-data = _data;
+    uint256 data;
+
+    constructor(uint256 _data) public {
+        data = _data;
+    }
+
+    function getresult() public view returns (uint256) {
+        return data;
+    }
 }
-function getresult()public view returns(uint){
-return data;
-}
-}
+
 contract Derived is Base {
-constructor(uint _info) Base(_info * _info) public {}
+    constructor(uint256 _info) public Base(_info * _info) {}
 }
+
 
 
 
 #D)Abstract Contracts:
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
 
-
-contract Calculator {
-function getResult() public view returns(uint);
+abstract contract Calculator {
+    function getResult() public view virtual returns (uint256) {
+        return 0;  // Default implementation
+    }
 }
+
 contract Test is Calculator {
-function getResult() public view returns(uint) {
-uint a = 4;
-uint b = 2;
-uint result = a + b;
-return result;
+    function getResult() public view override returns (uint256) {
+        uint256 a = 4;
+        uint256 b = 2;
+        uint256 result = a + b;
+        return result;
+    }
 }
-}
+
 
 
 
 #E)Interfaces:
-pragma solidity ^0.5.0;
-
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
 
 interface Calculator {
-function getResult() external view returns(uint);
+    function getResult() external view returns (uint256);
 }
+
 contract Test is Calculator {
-constructor() public {}
-function getResult() external view returns(uint){
-uint a = 5;
-uint b = 2;
-uint result = a + b;
-return result;
+    constructor() {}
+
+    function getResult() external pure override returns (uint256) {
+        uint256 a = 5;
+        uint256 b = 2;
+        uint256 result = a + b;
+        return result;
+    }
 }
-}
+
