8085d0bbdf2b50d4c0ffa7238c1f30c6d09045ff
Notcool404 <nishantdhuri404@gmail.com>
2025-04-04 02:35:31 +0530

    Update prac6c_libraries_assembly.py

--- prac6c_libraries_assembly.py
+++ prac6c_libraries_assembly.py
@@ -61,26 +61,21 @@ return Sum.sumUsingInlineAssembly(data);
 #H)Events:
 // Solidity program to demonstrate
 // creating an event
-pragma solidity ^0.4.21;
-
+pragma solidity ^0.8.21;
 
 // Creating a contract
 contract eventExample {
+    // Declaring state variables
+    uint256 public value = 0;
 
+    // Declaring an event
+    event Increment(address owner);
 
-// Declaring state variables
-uint256 public value = 0;
-
-
-// Declaring an event
-event Increment(address owner);
-
-
-// Defining a function for logging event
-function getValue(uint _a, uint _b) public {
-emit Increment(msg.sender);
-value = _a + _b;
-}
+    // Defining a function for logging event
+    function getValue(uint256 _a, uint256 _b) public {
+        emit Increment(msg.sender);
+        value = _a + _b;
+    }
 }
 
 
